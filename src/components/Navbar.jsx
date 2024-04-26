21a41a4b7aa0776087b936398de98f8d8b77b3be
Vedant-29 <agarwalvedant42@gmail.com>
2024-04-27 04:17:51 +0530

    home page -- responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,15 @@ function Navbar() {
 
   return (
     <nav className="bg-white p-3">
-      <div className="container mx-auto flex items-center justify-between flex-wrap ">
+      <div className="container mx-auto flex items-center justify-between flex-wrap px-4 sm:px-5">
         <div className="flex items-center flex-shrink-0 text-black mr-6">
           <span className="font-semibold text-xl tracking-tight">Logo</span>
         </div>
-        <div>
+        <div className="flex flex-row">
           {user ? (
             <Link
               to="/profile"
-              className="inline-block text-md px-4 py-3 leading-none rounded text-[#0BBD52] font-bold bg-[#E2F9E3] mt-4 lg:mt-2"
+              className="inline-block text-sm sm:text-md px-2 sm:px-4 py-2 sm:py-3 leading-none rounded text-[#0BBD52] font-bold bg-[#E2F9E3] mt-2 sm:mt-0"
             >
               Profile
             </Link>
@@ -23,13 +23,13 @@ function Navbar() {
             <>
               <Link
                 to="/sign-in"
-                className="inline-block text-md px-4 py-3 leading-none rounded font-bold text-[#0B76BD] mt-4 lg:mt-0 mr-4"
+                className="inline-block text-sm sm:text-md px-2 sm:px-4 py-2 sm:py-3 leading-none rounded font-bold text-[#0B76BD] mt-2 sm:mt-0 mr-2 sm:mr-4"
               >
                 Sign in
               </Link>
               <Link
                 to="/sign-up"
-                className="inline-block text-md px-4 py-3 leading-none rounded text-[#0B76BD] font-bold bg-[#E2F1F9] mt-4 lg:mt-0 "
+                className="inline-block text-sm sm:text-md px-2 sm:px-4 py-2 sm:py-3 leading-none rounded text-[#0B76BD] font-bold bg-[#E2F1F9] mt-2 sm:mt-0 "
               >
                 Sign up
               </Link>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -57,99 +57,99 @@ function HomePage() {
 
   return (
     <div className="h-screen w-full scrollbar-hide">
-      <div className="flex-col justify-center items-center">
-        <div className="flex-col justify-center items-center container mx-auto mt-4">
-          <h1 className="text-4xl font-semibold text-center text-black">
-            Our Swimming Centers
-          </h1>
-          <div className="flex flex-wrap items-center max-w-3xl justify-center mx-auto mt-5">
-            {locations.map((location) => (
-              <button
-                key={location.location_id}
-                className={`m-2 px-4 py-2 border-2 rounded-3xl ${
-                  selectedLocation === location.location_name
-                    ? "bg-[#E2F1F9] text-[#0B76BD] font-semibold border-[#E2F1F9]"
-                    : "bg-white text-black font-normal border-[#F5F5F5]"
-                }`}
-                onClick={() =>
-                  handleLocationClick(
-                    location.location_id,
-                    location.location_name
-                  )
-                }
-              >
-                {location.location_name}
-              </button>
-            ))}
-          </div>
-          <div className="max-w-4xl mx-auto mt-10">
-            <div className="flex text-2xl font-medium text-center text-black">
-              Our Centers
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 py-10">
-              {swimmingCenters.map((center) => (
-                <SwimmingCenterCard
-                  key={center.center_id}
-                  center={center}
-                  onClick={() => handleCenterClick(center.center_id)}
-                />
-              ))}
-            </div>
+  <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center container mx-auto mt-4 px-4 sm:px-0">
+      <h1 className="text-2xl sm:text-4xl font-semibold text-center text-black">
+        Our Swimming Centers
+      </h1>
+      <div className="flex flex-wrap items-center lg:max-w-3xl justify-center mx-auto mt-5">
+        {locations.map((location) => (
+          <button
+            key={location.location_id}
+            className={`m-2 px-3 sm:px-4 py-1 sm:py-2 border-2 rounded-3xl ${
+              selectedLocation === location.location_name
+                ? "bg-[#E2F1F9] text-[#0B76BD] font-semibold border-[#E2F1F9]"
+                : "bg-white text-black font-normal border-[#F5F5F5]"
+            }`}
+            onClick={() =>
+              handleLocationClick(
+                location.location_id,
+                location.location_name
+              )
+            }
+          >
+            {location.location_name}
+          </button>
+        ))}
+      </div>
+      <div className="max-w-full sm:max-w-4xl mx-auto mt-10 px-4 sm:px-0">
+        <div className="flex text-xl sm:text-2xl font-medium text-center text-black">
+          Our Centers
+        </div>
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-3 py-10">
+          {swimmingCenters.map((center) => (
+            <SwimmingCenterCard
+              key={center.center_id}
+              center={center}
+              onClick={() => handleCenterClick(center.center_id)}
+            />
+          ))}
+        </div>
 
-            {selectedCenter && (
-              <div className="rounded-lg bg-transparent mt-4">
-                <div className="flex text-2xl font-medium text-center text-black">
-                  Our Plans
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6 py-10">
-                  {plan.map((p) => (
-                    <div
-                      key={p.plan_id}
-                      onClick={() => setIsDivClicked(p.plan_id)}
-                    >
-                      <div
-                        className={`border-0 outline-none shadow-custom-light hover:shadow-custom-dark transition-shadow duration-300 ${
-                          isDivClicked === p.plan_id
-                            ? "border-0 outline-none shadow-custom-light hover:shadow-custom-dark transition-shadow duration-300 rounded-t-xl"
-                            : "rounded-xl"
-                        }`}
-                      >
-                        <div className="w-full p-4 flex items-center">
-                          <div className="flex-col items-start">
-                            <h1 className="text-lg font-medium">
-                              {p.plan_name}
-                            </h1>
-                            <div className="flex items-center ">
-                              <div className="focus:outline-none bg-transparent font-normal text-[#3B82F6] px-1 text-md py-1 rounded-sm mr-0.5 line-through">
-                                ${p.cutoff_plan_price}
-                              </div>
-                              <div className="focus:outline-none bg-transparent font-semibold text-[#3B82F6] pr-1 text-xl py-1 rounded-sm mr-0.5">
-                                ${p.plan_price}
-                              </div>
-                              <div className="bg-[#16A34A] text-white text-xs px-2 py-0.5 rounded-full ml-2 cursor-default">
-                                {p.discount * 100}% OFF
-                              </div>
-                            </div>
-                            <h1 className="px-1 text-[#A0A0A0] text-xs font-normal">
-                              + ({p.tax * 100}% GST)
-                            </h1>
+        {selectedCenter && (
+          <div className="rounded-lg bg-transparent mt-4">
+            <div className="flex text-xl sm:text-2xl font-medium text-center text-black">
+              Our Plans
+            </div>
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-3 py-10">
+              {plan.map((p) => (
+                <div
+                  key={p.plan_id}
+                  onClick={() => setIsDivClicked(p.plan_id)}
+                >
+                  <div
+                    className={`border-0 outline-none shadow-custom-light hover:shadow-custom-dark transition-shadow duration-300 ${
+                      isDivClicked === p.plan_id
+                        ? "border-0 outline-none shadow-custom-light hover:shadow-custom-dark transition-shadow duration-300 rounded-t-xl"
+                        : "rounded-xl"
+                    }`}
+                  >
+                    <div className="w-full p-4 flex items-center">
+                      <div className="flex-col items-start">
+                        <h1 className="text-lg font-medium">
+                          {p.plan_name}
+                        </h1>
+                        <div className="flex items-center ">
+                          <div className="focus:outline-none bg-transparent font-normal text-[#3B82F6] px-1 text-md py-1 rounded-sm mr-0.5 line-through">
+                            ${p.cutoff_plan_price}
+                          </div>
+                          <div className="focus:outline-none bg-transparent font-semibold text-[#3B82F6] pr-1 text-xl py-1 rounded-sm mr-0.5">
+                            ${p.plan_price}
+                          </div>
+                          <div className="bg-[#16A34A] text-white text-xs px-2 py-0.5 rounded-full ml-2 cursor-default">
+                            {p.discount * 100}% OFF
                           </div>
                         </div>
+                        <h1 className="px-1 text-[#A0A0A0] text-xs font-normal">
+                          + ({p.tax * 100}% GST)
+                        </h1>
                       </div>
-                      {isDivClicked === p.plan_id && (
-                        <button className="w-full bg-[#E75A5A] text-white py-2 rounded-b-xl mt-auto font-semibold">
-                          Book Now
-                        </button>
-                      )}
                     </div>
-                  ))}
+                  </div>
+                  {isDivClicked === p.plan_id && (
+                    <button className="w-full bg-[#E75A5A] text-white py-2 rounded-b-xl mt-auto font-semibold">
+                      Book Now
+                    </button>
+                  )}
                 </div>
-              </div>
-            )}
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
+  </div>
+</div>
   );
 }
 
