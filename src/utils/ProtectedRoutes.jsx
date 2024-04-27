2b2748182d9fadf8799777f958ff8448571295b6
Vedant-29 <agarwalvedant42@gmail.com>
2024-04-27 11:28:27 +0530

    Back button -- 5

--- src/utils/ProtectedRoutes.jsx
+++ src/utils/ProtectedRoutes.jsx
@@ -1,25 +1,24 @@
-import { Navigate } from "react-router-dom"
 import { useAuth } from "../hooks/auth"
 import { useEffect } from "react"
-
+import { useNavigate } from "react-router-dom"
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useAuth();
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const handleBackButton = () => {
+      navigate(-1);
+    } 
+
+    window.addEventListener('popstate', handleBackButton);
+
+    return () => {
+      window.removeEventListener('popstate', handleBackButton);
+    };
+  }, []);
 
   if (!user) {
-    useEffect(() => {
-      const handleBackButton = () => {
-        navigate(-1);
-      } 
-  
-      window.addEventListener('popstate', handleBackButton);
-  
-      return () => {
-        window.removeEventListener('popstate', handleBackButton);
-      };
-    }, []);
     // user is not authenticated
     return <Navigate to="/sign-up" />
   }
